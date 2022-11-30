cc3ccff5f5114dca14016a88ee49dd7d1e060558
Park <pbk95120@gmail.com>
2022-11-30 19:51:44 +0900

    second api commit fix2

--- master.js
+++ master.js
@@ -113,6 +113,14 @@ add_location.addEventListener("click", () => {
             modalOff();
         })
 
+        const gwangju = modal.querySelector("#gwangju")
+        gwangju.addEventListener("click", e => {
+            city = 'gwangju'
+            city_input();    
+            modalOff();
+        })
+
+
         const mokpo = modal.querySelector("#mokpo")
         mokpo.addEventListener("click", e => {
             city = 'mokpo'

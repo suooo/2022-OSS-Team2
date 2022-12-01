4ea5837e2ede2557efc688de30157bdffc744133
suooo <paksy0428@gmail.com>
2022-12-02 06:51:15 +0900

    modify css

--- memo.js
+++ memo.js
@@ -1,4 +1,3 @@
-
 var add_button = document.querySelector("#add");
 
 var memo_box = document.querySelector("#memo_box");
@@ -19,30 +18,35 @@ var month = [
   "Dec",
 ];
 
-function update(){
-  memo_box.innerHTML = '';
+function update() {
+  memo_box.innerHTML = "";
   map.clear();
-  for(let i = 0; i < localStorage.length; i++) {
+  for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
     map.set(key, value);
   }
   const temp_map = [...map].sort().reverse();
   for (const [key, val] of temp_map) {
-    let stoarr= key.split(' ');
+    let stoarr = key.split(" ");
     let m = month[stoarr[0] - 1];
-    let date = ' '+ stoarr[1];
+    let date = " " + stoarr[1];
     let dummy = document.createElement("div");
-    let temperature = val.split('_', 1)[0];
+    let temperature = val.split("_", 1)[0];
     let value = val.substring(4);
-    let variable = `` +
+    let variable =
+      `` +
       `<label class="memo_label">` +
       `    <input class="memo_checkbox" type="checkbox" name="" value="">` +
       `    <div class="flip_card">` +
       `        <div class="front memo_date">` +
-      `            <button class="btn_delete" type="button" name="${key}" onclick='OnClickRemoveButton(this)'><img src="/img/Clear.png" alt=""></button>` +
+      `            <button class="btn_delete" type="button" name="${key}" onclick='OnClickRemoveButton(this)'></button>` +
       `            <div class="wow" style="padding: 10px;">` +
-      `                <h3 id="date">${m + date}<span style="color:rgba(255, 255, 255, 0.5);"> ${temperature + "°"}</span></h3>` +
+      `                <h3 id="date">${
+        m + date
+      }<span style="color:rgba(255, 255, 255, 0.5);"> ${
+        temperature + "°"
+      }</span></h3>` +
       `            </div>` +
       `        </div>` +
       `        <div class="back memo_data">` +
@@ -51,52 +55,54 @@ function update(){
       `    </div>` +
       `</label>` +
       ``;
-      dummy.innerHTML = variable;
-      memo_box.appendChild(dummy);
+    dummy.innerHTML = variable;
+    memo_box.appendChild(dummy);
   }
 }
 
-window.onload = function() {
+window.onload = function () {
   update();
-}
+};
 
 add_button?.addEventListener("click", () => {
   // 1. Read the text in #task-input.
   let input = document.querySelector("#memoInput");
   let value = input.value;
-  input.value = '';
-  if(value.length <= 3){
-    alert("You must enter at least 3 characters.")
+  input.value = "";
+  if (value.length <= 3) {
+    alert("You must enter at least 3 characters.");
     return;
   }
 
-  let temperature = document.querySelector("#temp_cur").innerHTML;//
-  while(temperature.length < 4)
-    temperature+="_";
+  let temperature = document.querySelector("#temp_cur").innerHTML; //
+  while (temperature.length < 4) temperature += "_";
 
-  let key1 = `${theDate.getMonth() + 1}`  // today's date variable
-  if(key1 < 10)
-    key1 = '0' + key1;
-  let key2 = `${theDate.getDate()}`;   // testable variable
-  if(key2 < 10)
-    key2 = '0' + key2;
-  let key = key1 + ' ' + key2;
-  let stoarr= key.split(' ');
+  let key1 = `${theDate.getMonth() + 1}`; // today's date variable
+  if (key1 < 10) key1 = "0" + key1;
+  let key2 = `${theDate.getDate()}`; // testable variable
+  if (key2 < 10) key2 = "0" + key2;
+  let key = key1 + " " + key2;
+  let stoarr = key.split(" ");
   let m = month[stoarr[0] - 1];
-  let date = ' '+ stoarr[1];
-  if(map.has(key)){
-    if (confirm(`A memo is already written on ${m + date}.\nDo you want to overwrite it?`))
+  let date = " " + stoarr[1];
+  if (map.has(key)) {
+    if (
+      confirm(
+        `A memo is already written on ${
+          m + date
+        }.\nDo you want to overwrite it?`
+      )
+    )
       localStorage.removeItem(key);
-    else
-      return;
+    else return;
   }
   localStorage.setItem(key, temperature + value);
 
   update();
 });
 
-function OnClickRemoveButton(ths){
-  let key = ths.getAttribute('name');;
+function OnClickRemoveButton(ths) {
+  let key = ths.getAttribute("name");
   localStorage.removeItem(key);
 
   update();
